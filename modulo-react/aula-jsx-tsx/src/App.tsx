def53e89ad2c25445b6d3a293ba83c54462e18c8
pablotq <pablo.cstds@gmail.com>
2025-10-22 09:56:12 -0300

    aula de tsx

---
+++ modulo-react/aula-jsx-tsx/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  const name = 'Pablo';
+
+  return (
+    <>
+      <h1>Olá, {name}!</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat id provident beatae quidem distinctio voluptatum officia laboriosam cupiditate neque natus optio nam sint sit harum, quam aspernatur eligendi, minus illo!</p>
+    </>
+  )
+}
+
+export default App
