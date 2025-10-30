3645d25f92ce0e95da2e260da4088b533776f392
pablotq <pablo.cstds@gmail.com>
2025-10-30 08:33:19 -0300

    usando tailwind no projeto

--- modulo-react/todolist-app/src/App.tsx
+++ modulo-react/todolist-app/src/App.tsx
@@ -3,8 +3,15 @@
 function App() {
 
   return (
-    <>
-    </>
+    <div className="bg-orange-500 flex justify-evenly h-100 items-center">
+      
+      <div className="bg-[#6528D3] w-[125px] h-60 m-4 sm:bg-white lg:bg-green-500" >
+       
+      </div>
+      <div className="bg-red-500 w-40 h-[250px] m-4">
+         <p className="text-blue-800 text-[12px] text-center">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae doloremque doloribus officia, facere aspernatur quia et, vel dolorem consectetur a rerum dicta nisi perferendis repellat, incidunt odio obcaecati ab quibusdam.</p>
+      </div>
+    </div>
   )
 }
 
