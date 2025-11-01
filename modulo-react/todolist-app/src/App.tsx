b4186eabc7b1c625aa755636df1f1cd56c64148b
pablotq <pablo.cstds@gmail.com>
2025-10-31 21:09:19 -0300

    finalização estilização e criação do  TodoContainer

--- modulo-react/todolist-app/src/App.tsx
+++ modulo-react/todolist-app/src/App.tsx
@@ -3,26 +3,18 @@ import TodoForm from './components/TodoForm'
 import TodoHeader from './components/TodoHeader'
 import TodoList from './components/TodoList';
 
-import { useContext } from 'react';
-import { themeConfig } from './contexts/theme'
-import { ThemeContext } from './contexts/ThemeContext';
+import { TodoContainer } from './components/TodoContainer';
 
 function App() {
-  
-    const {theme} = useContext(ThemeContext);
+
 
   return (
     <>
-      <main className={`${themeConfig[theme].layout.backgroundColor} h-screen`}>
-        <div className={`${themeConfig[theme].layout.imageBanner} h-80 bg-cover bg-center `}>
-          <div className='max-w-175 m-auto p-8'>
-            <TodoHeader></TodoHeader>
-            <TodoForm />
-            <TodoList></TodoList>
-            
-          </div>
-        </div>
-      </main>
+      <TodoContainer>
+        <TodoHeader></TodoHeader>
+        <TodoForm />
+        <TodoList></TodoList>
+      </TodoContainer>
     </>
   )
 }
--- modulo-react/todolist-app/src/components/TodoList/index.tsx
+++ modulo-react/todolist-app/src/components/TodoList/index.tsx
@@ -11,6 +11,8 @@ const todos = [
 const TodoList = () => {
   const { theme } = useContext(ThemeContext);
 
+  const hoverBottomButton = theme === "dark" ? "hover:text-dark-purple-100-hover" : "hover:text-light-navy-850";
+
   return (
     <>
       <div className={`${themeConfig[theme].todo.backgroundColor} rounded-md`}>
@@ -19,7 +21,9 @@ const TodoList = () => {
             todos.map((todo) => (
               <li className={`p-6 border-b ${themeConfig[theme].todo.borderColor}`} key={todo.id}>
                 <div className='flex items-center gap-4'>
-                  <button className={`w-6 h-6 border ${themeConfig[theme].todo.borderColor} rounded-full cursor-pointer`}></button>
+                  <span className='w-6 h-6 rounded-full hover:bg-[linear-gradient(to_right,hsl(192,100%,67%),hsl(280,87%,65%))] hover:p-px'>
+                    <button className={`w-full h-full border ${themeConfig[theme].todo.borderColor} rounded-full cursor-pointer ${themeConfig[theme].todo.backgroundColor}`}></button>
+                  </span>
                   <p className={`${themeConfig[theme].todo.textColor}`}>{todo.text}</p>
                 </div>
               </li>
@@ -29,18 +33,18 @@ const TodoList = () => {
         <div className={`text-sm flex justify-between p-4 ${themeConfig[theme].layout.textColor}`}>
           <p>{todos.length} items total</p>
           <div className='hidden sm:flex gap-4'>
-            <button className='text-bright-blue cursor-pointer'>All</button>
-            <button className='cursor-pointer'>Active</button>
-            <button className='cursor-pointer'>Completed</button>
+            <button className='text-bright-blue cursor-pointer hover:text-[hsl(192,100%,67%)]'>All</button>
+            <button className={`cursor-pointer ${hoverBottomButton}`}>Active</button>
+            <button className={`cursor-pointer ${hoverBottomButton}`}>Completed</button>
           </div>
-          <button className='cursor-pointer'>Clear Comlpeted</button>
+          <button className={`cursor-pointer ${hoverBottomButton}`}>Clear Comlpeted</button>
         </div>
 
       </div>
       <div className={`flex sm:hidden justify-center gap-5 py-4 mt-4 rounded-md ${themeConfig[theme].layout.textColor} ${themeConfig[theme].todo.backgroundColor} `}>
-        <button className='text-bright-blue cursor-pointer'>All</button>
-        <button className='cursor-pointer'>Active</button>
-        <button className='cursor-pointer'>Completed</button>
+        <button className='text-bright-blue cursor-pointer hover:text-[hsl(192,100%,67%)]'>All</button>
+        <button className={`cursor-pointer ${hoverBottomButton}`}>Active</button>
+        <button className={`cursor-pointer ${hoverBottomButton}`}>Completed</button>
       </div>
     </>
   );
