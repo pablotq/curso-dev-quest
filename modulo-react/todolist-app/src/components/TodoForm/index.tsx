897fcb7639c3352d095462472e698ef6879f0013
pablotq <pablo.cstds@gmail.com>
2025-11-03 17:57:21 -0300

    adicionado a lógica do todo list

--- modulo-react/todolist-app/src/components/TodoForm/index.tsx
+++ modulo-react/todolist-app/src/components/TodoForm/index.tsx
@@ -1,14 +1,19 @@
-import { useContext } from 'react';
+import { useContext, type FormEvent } from 'react';
 import { themeConfig } from '../../contexts/theme'
 import { ThemeContext } from '../../contexts/ThemeContext';
 
-const TodoForm = () =>{
+interface TodoInputProps{
+    addTodo: (event: FormEvent<HTMLFormElement>) => void
+};
+
+const TodoForm = ({addTodo}: TodoInputProps) =>{
     const {theme} = useContext(ThemeContext);
 
     return(
-        <form className='relative mb-10'>
+        <form className='relative mb-10' onSubmit={addTodo}>
               <span className={` absolute w-6 h-6 border  ${themeConfig[theme].todo.borderColor} top-1/2 transform -translate-y-1/2 rounded-full left-6`}></span>
-              <input type='text' placeholder='Create a new todo...' className={`${themeConfig[theme].todo.backgroundColor} w-full ${themeConfig[theme].todo.textColor} rounded-md py-6 pl-16 outline-none text-lg`} />
+
+              <input type='text' placeholder='Create a new todo...' name='todo' className={`${themeConfig[theme].todo.backgroundColor} w-full ${themeConfig[theme].todo.textColor} rounded-md py-6 pl-16 outline-none text-lg`} />
             </form>
     );
 };
--- modulo-react/todolist-app/src/components/TodoList/index.tsx
+++ modulo-react/todolist-app/src/components/TodoList/index.tsx
@@ -1,14 +1,21 @@
 import { useContext } from 'react';
 import { themeConfig } from '../../contexts/theme'
 import { ThemeContext } from '../../contexts/ThemeContext';
+import type { Todo } from '../../hooks/useTodo';
+import IconCheck from '/images/icon-check.svg';
+import TodoListTypeButtons from '../TodoListTypeButtons';
 
-const todos = [
-  { id: 1, text: 'Todo 1' },
-  { id: 2, text: 'Todo 2' },
-  { id: 3, text: 'Todo 3' },
-];
+interface TodoListProps {
+  todoList: Todo[];
+  toggleTodoCompleted: (id: number) => void;
+  setFilter: (filter: 'all' | 'active' | 'completed') => void;
+  filter: 'all' | 'active' | 'completed';
+  clearCompleted: () => void;
+  removeTodo: (id: number) => void;
+}
 
-const TodoList = () => {
+
+const TodoList = ({ todoList, toggleTodoCompleted, setFilter, filter, clearCompleted, removeTodo }: TodoListProps) => {
   const { theme } = useContext(ThemeContext);
 
   const hoverBottomButton = theme === "dark" ? "hover:text-dark-purple-100-hover" : "hover:text-light-navy-850";
@@ -18,33 +25,50 @@ const TodoList = () => {
       <div className={`${themeConfig[theme].todo.backgroundColor} rounded-md`}>
         <ul>
           {
-            todos.map((todo) => (
+            todoList.map((todo) => (
               <li className={`p-6 border-b ${themeConfig[theme].todo.borderColor}`} key={todo.id}>
                 <div className='flex items-center gap-4'>
                   <span className='w-6 h-6 rounded-full hover:bg-[linear-gradient(to_right,hsl(192,100%,67%),hsl(280,87%,65%))] hover:p-px'>
-                    <button className={`w-full h-full border ${themeConfig[theme].todo.borderColor} rounded-full cursor-pointer ${themeConfig[theme].todo.backgroundColor}`}></button>
+
+                    <button className={`w-full h-full border ${themeConfig[theme].todo.borderColor} rounded-full cursor-pointer ${themeConfig[theme].todo.backgroundColor} ${todo.completed ? 'bg-[linear-gradient(to_right,hsl(192,100%,67%),hsl(280,87%,65%))]' : ''}`}
+                      onClick={() => toggleTodoCompleted(todo.id)}
+                    >
+
+                      {todo.completed && (
+                        <img src={IconCheck} alt="íconde de marcado" className='h-2 w-2 m-auto' />
+                      )}
+
+                    </button>
                   </span>
-                  <p className={`${themeConfig[theme].todo.textColor}`}>{todo.text}</p>
+                  <div className='flex w-full justify-between hover:cursor-pointer group'
+                  onClick={() => removeTodo(todo.id)}>
+                    <p className={`${themeConfig[theme].todo.textColor} ${todo.completed ? "line-through opacity-60" : ''}`}
+                    >{todo.text}</p>
+
+                    <button
+                      className={`opacity-0 hover:cursor-pointer group-hover:opacity-100 transition-opacity duration-150  ${themeConfig[theme].layout.textColor}`}
+                      >X</button>
+                  </div>
                 </div>
               </li>
             ))
           }
         </ul>
         <div className={`text-sm flex justify-between p-4 ${themeConfig[theme].layout.textColor}`}>
-          <p>{todos.length} items total</p>
+          <p>{todoList.length} items total</p>
           <div className='hidden sm:flex gap-4'>
-            <button className='text-bright-blue cursor-pointer hover:text-[hsl(192,100%,67%)]'>All</button>
-            <button className={`cursor-pointer ${hoverBottomButton}`}>Active</button>
-            <button className={`cursor-pointer ${hoverBottomButton}`}>Completed</button>
+            <TodoListTypeButtons setFilter={setFilter}
+              filter={filter} />
           </div>
-          <button className={`cursor-pointer ${hoverBottomButton}`}>Clear Comlpeted</button>
+          <button
+            className={`cursor-pointer ${hoverBottomButton}`}
+            onClick={clearCompleted}>Clear Comlpeted</button>
         </div>
 
       </div>
       <div className={`flex sm:hidden justify-center gap-5 py-4 mt-4 rounded-md ${themeConfig[theme].layout.textColor} ${themeConfig[theme].todo.backgroundColor} `}>
-        <button className='text-bright-blue cursor-pointer hover:text-[hsl(192,100%,67%)]'>All</button>
-        <button className={`cursor-pointer ${hoverBottomButton}`}>Active</button>
-        <button className={`cursor-pointer ${hoverBottomButton}`}>Completed</button>
+        <TodoListTypeButtons setFilter={setFilter}
+          filter={filter} />
       </div>
     </>
   );
