89fa463192d239421bc27cf4cfb7e11c9ba499f5
pablotq <pablo.cstds@gmail.com>
2025-11-13 16:28:36 -0300

    aula de React Hook Forms e Zod

---
+++ modulo-react-avancado/react-hook-forms-com-zod/src/components/Form/index.tsx
@@ -0,0 +1,67 @@
+import "./Form.css";
+import { z } from 'zod';
+import { useForm, type SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const passwordSchema = z
+  .string()
+  .min(6, { message: "A senha deve ter pelo menos 6 caracteres" })
+  .refine((val) => /[A-Z]/.test(val), { error: "A senha deve conter letra maiúscula" })
+  .refine((val) => /[a-z]/.test(val), { error: "A senha deve conter letra minúscula" })
+  .refine((val) => /[0-9]/.test(val), { error: "A senha deve conter número" })
+  .refine((val) => /[!@#$%^&*]/.test(val), { error: "A senha deve conter caractere especial" });
+
+
+
+const registerUserForSchema = z.object({
+    email: z.email('Preencha o e-mail corretamente').min(1, 'Campo Obrigatório'),
+    password: passwordSchema,
+    confirmPassword: z.string().min(6, 'A senha precisa ter no mínimo 6 caracteres')
+})
+   .refine((data) => data.password === data.confirmPassword, {
+        message: "As senhas devem ser iguais",
+        path: ["confirmPassword"],
+    });
+
+
+type registerUserFormData = z.infer<typeof registerUserForSchema>
+
+
+export const Form = () => {
+
+    const {
+        handleSubmit,
+        register,
+        formState: { errors, isSubmitting }
+    } = useForm<registerUserFormData>({ //tipar o useForm permite que ao usar o register use apenas argumentos válidos
+        mode: 'onBlur', // quando sair do input, fará a validação
+        criteriaMode: 'all', // pegar todos os erros
+        resolver: zodResolver(registerUserForSchema) // define que quem fará as verificações será o zod
+    })
+
+    const onSubmit: SubmitHandler<registerUserFormData> = async (data: registerUserFormData) => {
+        await fetch('https://localhost:3333/users', {
+            method: 'POST',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify(data)
+        })
+    }    
+
+    return (
+        <form className="container" onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor="email">E-mail</label>
+            <input type="email" id="email" placeholder="Informe seu e-mail" {...register("email")} />
+            {errors?.email && <p>{errors?.email.message}</p>}
+
+            <label htmlFor="password">Senha</label>
+            <input type="password" id="password" placeholder="Informe sua senha" {...register("password")} />
+            {errors?.password && <p>{errors?.password.message}</p>}
+
+            <label htmlFor="confirmPassword">Confirmar senha</label>
+            <input type="password" id="confirmPassword" placeholder="Informe sua senha novamente" {...register("confirmPassword")} />
+            {errors?.confirmPassword && <p>{errors?.confirmPassword.message}</p>}
+
+            <button type="submit" disabled={isSubmitting}>Cadastrar</button>
+        </form>
+    )
+}
